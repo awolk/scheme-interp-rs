55bd602ccec7ed2c1bbd2657025755c8d5da1f03
Aidan Wolk <aidanwolk@comcast.net>
2020-02-16 23:45:24 -0800

    Add lambda and if expressions

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -69,6 +69,8 @@ const UNBOUND_SYMBOL_ERROR: &str = "unbound symbol";
 const EVAL_EMPTY_LIST_ERROR: &str = "cannot evaluate empty list";
 const INVALID_FUNCTION_ERROR: &str = "attempt to call a non-function value";
 const WRONG_NUMBER_ARGS_ERROR: &str = "wrong number of arguments";
+const INVALID_IF_ERROR: &str = "invalid structure for if expression";
+const INVALID_LAMBDA_ERROR: &str = "invalid structure for lambda expression";
 
 type Continuation<'a> = Box<dyn FnOnce(Result<Rc<Value>, Error>) + 'a>;
 
@@ -93,6 +95,78 @@ fn eval_node<'a>(node: &'a AST, env: Rc<Environment>, cont: Continuation<'a>) {
                 return;
             }
 
+            // handle special forms
+            if let AST::Symbol(first_sym) = &nodes[0] {
+                match first_sym.as_str() {
+                    "if" => {
+                        if nodes.len() != 4 {
+                            cont(Err(Error {
+                                error_message: INVALID_IF_ERROR.to_string(),
+                            }));
+                            return;
+                        }
+
+                        eval_node(
+                            &nodes[1],
+                            Rc::clone(&env),
+                            Box::new(move |res| {
+                                if res.is_err() {
+                                    cont(res);
+                                    return;
+                                }
+
+                                let res = res.unwrap();
+                                if let Value::Bool(true) = res.as_ref() {
+                                    eval_node(&nodes[2], env, cont)
+                                } else {
+                                    eval_node(&nodes[3], env, cont)
+                                }
+                            }),
+                        );
+                        return;
+                    }
+                    "lambda" => {
+                        if nodes.len() != 3 {
+                            cont(Err(Error {
+                                error_message: INVALID_LAMBDA_ERROR.to_string(),
+                            }));
+                            return;
+                        }
+
+                        let mut args_names: Vec<String>;
+                        if let AST::List(arg_list) = &nodes[1] {
+                            args_names = Vec::with_capacity(arg_list.len());
+                            for arg in arg_list {
+                                if let AST::Symbol(arg) = arg {
+                                    args_names.push(arg.clone());
+                                } else {
+                                    cont(Err(Error {
+                                        error_message: INVALID_LAMBDA_ERROR.to_string(),
+                                    }));
+                                    return;
+                                }
+                            }
+                        } else {
+                            cont(Err(Error {
+                                error_message: INVALID_LAMBDA_ERROR.to_string(),
+                            }));
+                            return;
+                        }
+
+                        cont(Ok(Value::Function(Function {
+                            args: args_names,
+                            env: Rc::clone(&env),
+                            // TODO: can this clone be removed?
+                            body: nodes[2].clone(),
+                        })
+                        .rc()));
+                        return;
+                    }
+                    _ => {}
+                }
+            }
+
+            // evaluate function
             fn eval_nodes(
                 mut vals: Vec<Rc<Value>>,
                 mut nodes: std::slice::Iter<AST>,
@@ -204,6 +278,67 @@ mod test {
                     assert_eq!(*b, true);
                 } else {
                     panic!("expected boolean result");
+                }
+            }),
+        )
+    }
+
+    #[test]
+    fn handles_if() {
+        // (if #t (if #f 1 2) 3) -> 2
+        let node = AST::List(vec![
+            AST::Symbol("if".to_string()),
+            AST::Bool(true),
+            AST::List(vec![
+                AST::Symbol("if".to_string()),
+                AST::Bool(false),
+                AST::Integer(1),
+                AST::Integer(2),
+            ]),
+            AST::Integer(3),
+        ]);
+
+        let env = Environment::new_with_bindings(HashMap::new());
+        eval_node(
+            &node,
+            env,
+            Box::new(|res| {
+                let res = res.unwrap();
+                if let Value::Integer(i) = res.as_ref() {
+                    assert_eq!(*i, 2);
+                } else {
+                    panic!("expected integer result");
+                }
+            }),
+        )
+    }
+
+    #[test]
+    fn handles_lambda() {
+        // ((lambda (x) (+ x 1)) 2) -> 3
+        let node = AST::List(vec![
+            AST::List(vec![
+                AST::Symbol("lambda".to_string()),
+                AST::List(vec![AST::Symbol("x".to_string())]),
+                AST::List(vec![
+                    AST::Symbol("+".to_string()),
+                    AST::Symbol("x".to_string()),
+                    AST::Integer(1),
+                ]),
+            ]),
+            AST::Integer(2),
+        ]);
+
+        let env = stdlib::build();
+        eval_node(
+            &node,
+            env,
+            Box::new(|res| {
+                let res = res.unwrap();
+                if let Value::Integer(i) = res.as_ref() {
+                    assert_eq!(*i, 3);
+                } else {
+                    panic!("expected integer result");
                 }
             }),
         )
